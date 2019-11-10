bb095c2d21bb034b5a7b871d759e0689dc83c559
Valentin Palade <vipalade@gmail.com>
2019-11-10 22:13:26 +0200

    keep learning about listviewx

--- ola/client/store/store_main_window.cpp
+++ ola/client/store/store_main_window.cpp
@@ -1,6 +1,8 @@
 #include "store_main_window.hpp"
+#include "ui_list_form.h"
 #include "ui_store_form.h"
 #include <QKeyEvent>
+#include <QPainter>
 
 #include "solid/system/log.hpp"
 
@@ -10,17 +12,49 @@ namespace store {
 
 namespace {
 const solid::LoggerT logger("ola::client::store::widget");
-}
+} //namespace
 
 struct MainWindow::Data {
-    Ui::StoreForm             form_;
+    ListModel     list_model_;
+    Ui::StoreForm store_form_;
+    Ui::ListForm  list_form_;
+    ItemDelegate  list_delegate_;
 };
+
+void ItemDelegate::paint(QPainter* painter, const QStyleOptionViewItem& option,
+    const QModelIndex& index) const
+{
+    painter->save();
+
+    painter->setRenderHint(QPainter::Antialiasing, true);
+    if (option.state & QStyle::State_Selected) {
+        painter->fillRect(option.rect, option.palette.highlight());
+    }
+    
+    painter->setBrush(QBrush(QColor(index.row() % 255, 0, 0)));
+    painter->drawEllipse(option.rect);
+
+    painter->drawText(QPoint(option.rect.x() + option.rect.width() / 2, option.rect.y() + option.rect.height() / 2), QString("Row%1").arg(index.row() + 1));
+
+    painter->restore();
+}
+QSize ItemDelegate::sizeHint(const QStyleOptionViewItem& option,
+    const QModelIndex&                                   index) const
+{
+    return QSize(200,100);
+}
 
 MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
     , pimpl_(solid::make_pimpl<Data>())
 {
-    pimpl_->form_.setupUi(this);
+    pimpl_->store_form_.setupUi(this);
+    pimpl_->list_form_.setupUi(pimpl_->store_form_.listWidget);
+    pimpl_->list_form_.listView->setFlow(QListView::Flow::LeftToRight);
+    pimpl_->list_form_.listView->setWrapping(true);
+    //pimpl_->list_form_.listView->setGridSize(QSize(1000,10));
+    pimpl_->list_form_.listView->setModel(&pimpl_->list_model_);
+    pimpl_->list_form_.listView->setItemDelegate(&pimpl_->list_delegate_);
     setWindowFlags(Qt::Drawer);
 
     installEventFilter(this);
@@ -38,6 +72,7 @@ void MainWindow::setUser(const std::string& _user)
 
 void MainWindow::start()
 {
+    pimpl_->store_form_.itemWidget->hide();
 }
 
 void MainWindow::onOffline(bool _b)
--- ola/client/store/store_main_window.hpp
+++ ola/client/store/store_main_window.hpp
@@ -4,10 +4,42 @@
 #include <QMainWindow>
 #include <functional>
 #include <string>
+#include <QAbstractListModel>
+#include <QStyledItemDelegate>
 
 namespace ola {
 namespace client {
 namespace store {
+
+class ListModel : public QAbstractListModel {
+    Q_OBJECT
+public:
+    ListModel(QObject* parent = nullptr)
+        : QAbstractListModel(parent)
+    {
+    }
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override
+    {
+        return 1000;
+    }
+
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override
+    {
+        if (role == Qt::DisplayRole) {
+            return QString("Row%1").arg(index.row() + 1);
+        }
+        return QVariant();
+    }
+};
+
+class ItemDelegate : public QStyledItemDelegate {
+    Q_OBJECT
+public:
+    void     paint(QPainter* painter, const QStyleOptionViewItem& option,
+            const QModelIndex& index) const override;
+    QSize    sizeHint(const QStyleOptionViewItem& option,
+           const QModelIndex&                     index) const override;
+};
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
