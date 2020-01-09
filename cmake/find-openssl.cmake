c525142c6a0b33fb14fcd6f58b21a8b108da2d86
Valentin Palade <vipalade@gmail.com>
2020-01-10 00:29:41 +0200

    using openssl dlls from ola-client

--- cmake/find-openssl.cmake
+++ cmake/find-openssl.cmake
@@ -1,3 +1,8 @@
 set(OPENSSL_ROOT_DIR ${EXTERNAL_DIR})
 
 find_package(OpenSSL)
+
+file( GLOB OPENSSL_SSL_DLL "${EXTERNAL_DIR}/bin/libssl-*.dll")
+file( GLOB OPENSSL_CRYPTO_DLL "${EXTERNAL_DIR}/bin/libcrypto-*.dll")
+
+message("OpenSSL DLLs ${OPENSSL_SSL_DLL} ${OPENSSL_CRYPTO_DLL}")
