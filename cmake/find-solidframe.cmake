a8a6b637728f6fa288baceb903a27859aef7d74d
Valentin Palade <vipalade@gmail.com>
2020-02-29 17:03:58 +0200

    fix find-solidframe.cmake

--- cmake/find-solidframe.cmake
+++ cmake/find-solidframe.cmake
@@ -1,4 +1,4 @@
-set(SolidFrame_DIR "${EXTERNAL_PATH}/lib/cmake/SolidFrame" CACHE PATH "SolidFrame CMake configuration dir")
+set(SolidFrame_DIR "${EXTERNAL_DIR}/lib/cmake/SolidFrame" CACHE PATH "SolidFrame CMake configuration dir")
 find_package(SolidFrame 5)
 
 if(SolidFrame_FOUND)
